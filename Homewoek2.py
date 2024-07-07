4fb1c8f65cea9e07ad8a370f3710034b854667ff
MisterKovacs <ural4eg@gmail.com>
2024-07-07 14:46:53 +0500

    Add files via upload

---
+++ Homewoek2.py
@@ -0,0 +1,5 @@
+completed_tasks = 12
+worktime = 1.5
+course_name = "Python"
+time_for_one_task = worktime/completed_tasks
+print("Курс:",course_name,",","всего задач:",completed_tasks,",","затрачено часов:", worktime,",","среднее время выполнения:",time_for_one_task,"часа.")
