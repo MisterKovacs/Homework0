aa7317eb7a5503630b69c87265080b2ee42c114b
MisterKovacs <ural4eg@gmail.com>
2024-07-11 22:12:45 +0500

    Update Homework6.py

--- Homework6.py
+++ Homework6.py
@@ -5,7 +5,9 @@ print(my_dict['b'])
 print(my_dict.get('x'))
 my_dict.update({'d':654,
                 'e':789})
-del my_dict['c']
+print(my_dict)
+z = my_dict.pop('c')
+print(z)
 print(my_dict)
     # Множества
 my_set = [1, 2, 3, 2, 1, 'String', 3, (1, 2, 3)]
